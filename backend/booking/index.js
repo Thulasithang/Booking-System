55a40ef52230aa29ae07765abb23830b4ea21878
Thulasithang <thulasithangnanenthiram@gmail.com>
2024-03-22 10:43:42 +0530

    booking database and model created

--- backend/booking/index.js
+++ backend/booking/index.js
@@ -3,11 +3,12 @@ const app = express();
 const PORT = process.env.PORT || 8002;
 
 const sequelize = require('./dbConfig');
+const Booking = require('./models/booking');
 
 app.use(express.json());
 
-const bookingController = require('./controllers/bookingController');
-app.use("/", bookingController);
+// const bookingController = require('./controllers/bookingController');
+// app.use("/", bookingController);
 
 // app.use("/", (req, res) =>{
 //     res.json({ message: "Booking service" });
@@ -16,3 +17,13 @@ app.use("/", bookingController);
 app.listen(PORT, () => {
     console.log(`Booking service started on port ${PORT}`);
 });
+
+
+// (async () => {
+//       try {
+//         await Booking.sync({ force: true }); // This will drop existing tables and recreate them
+//         console.log("Database synchronized successfully");
+//       } catch (error) {
+//         console.error("Error syncing database:", error);
+//       }
+//     })();
--- backend/booking/models/booking.js
+++ backend/booking/models/booking.js
@@ -13,51 +13,46 @@ const Booking = sequelize.define(
     user_id: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      // references: {
-      //     model: "user_account",
-      //     key: "user_id",
-      // },
     },
-    facility_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    booking_date: {
-      type: DataTypes.DATEONLY,
-      allowNull: false,
-      defaultValue: DataTypes.NOW,
-    },
-    booking_time: {
-      type: DataTypes.DATETIME,
-      allowNull: false,
-    },
-    duration: {
-      type: DataTypes.RANGE(DataTypes.DATETIME),
-      allowNull: false,
-    },
-    completed: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: false,
-    },
-    booked_at: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: DataTypes.NOW,
-    },
-    coach_required: {
+    need_coaching: {
       type: DataTypes.BOOLEAN,
       allowNull: false,
       defaultValue: false,
     },
     coach_id: {
       type: DataTypes.INTEGER,
-      allowNull: true,
+      allowNull: true, // if coach is not required, then coach_id is null
     },
-    coach_approved: {
-        type: DataTypes.BOOLEAN,
-        allowNull: true,
-        defaultValue: false,
+    facility_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true, // if coach is required, then facility_id is null
+    },
+    booked_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    date_booked: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+    slots_booked: {
+      type: DataTypes.ARRAY(DataTypes.TIME),
+      allowNull: false,
+    },
+    payment_status: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+    booking_status: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: "pending",   // pending, confirmed, cancelled by user, cancelled by coach
+    },
+    amount: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
     },
   },
   {
